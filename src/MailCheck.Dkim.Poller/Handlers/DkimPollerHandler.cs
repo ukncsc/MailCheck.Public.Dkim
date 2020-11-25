e8e2b8ea223adefc85a7ec0ccaf0552857c2b39b
NCSC Code Release <ncsc@git>
2020-11-25 12:48:24 +0000

    Release 2020-11-25

--- src/MailCheck.Dkim.Poller/Handlers/DkimPollerHandler.cs
+++ src/MailCheck.Dkim.Poller/Handlers/DkimPollerHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using MailCheck.Common.Messaging.Abstractions;
 using MailCheck.Dkim.Contracts.Entity;
@@ -30,23 +29,32 @@ namespace MailCheck.Dkim.Poller.Handlers
 
         public async Task Handle(DkimPollPending message)
         {
-            List<DkimSelectorRecords> dkimSelectorRecords =
-                await _dnsClient.FetchDkimRecords(message.Id, message.Selectors);
-
-            if (!_config.AllowNullResults && dkimSelectorRecords.TrueForAll(x =>
-                    x.Records.Count == 0 || x.Records.TrueForAll(y => string.IsNullOrWhiteSpace(y.Record))))
+            try
             {
-                throw new Exception($"Unable to retrieve DKIM records for {message.Id}, selectors: {JsonConvert.SerializeObject(message)}");
+                List<DkimSelectorRecords> dkimSelectorRecords =
+                    await _dnsClient.FetchDkimRecords(message.Id, message.Selectors);
+
+                if (!_config.AllowNullResults && dkimSelectorRecords.TrueForAll(x =>
+                         x.Records.Count == 0 || x.Records.TrueForAll(y => string.IsNullOrWhiteSpace(y.Record))))
+                {
+                    throw new Exception($"Unable to retrieve DKIM records for {message.Id}, selectors: {JsonConvert.SerializeObject(message)}");
+                }
+
+                DkimRecordsPolled dkimRecordsPolled = new DkimRecordsPolled(message.Id, dkimSelectorRecords);
+
+                _log.LogInformation("Polled DKIM selectors for {Domain}", message.Id);
+
+                _dispatcher.Dispatch(dkimRecordsPolled, _config.SnsTopicArn);
+
+                _log.LogInformation("Published DKIM records for {Domain}", message.Id);
+
             }
-
-            DkimRecordsPolled dkimRecordsPolled = new DkimRecordsPolled(message.Id, dkimSelectorRecords);
-
-            _log.LogInformation("Polled DKIM selectors for {Domain}", message.Id);
-
-            _dispatcher.Dispatch(dkimRecordsPolled, _config.SnsTopicArn);
-
-            _log.LogInformation("Published DKIM records for {Domain}", message.Id);
-
+            catch (Exception e)
+            {
+                string error = $"Error occurred polling domain {message.Id}";
+                _log.LogError(e, error);
+                throw;
+            }
         }
     }
 }
